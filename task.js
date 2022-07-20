ff1466437bf6f662b3e2301bb8b0103c514cd212
Роман Шапарь <romanshaparution@gmail.com>
2022-07-20 06:54:56 +0300

    modif

--- task.js
+++ task.js
@@ -1,11 +1,22 @@
+`use strict`
+
 function solveEquation(a, b, c) {
-    let arr;
-    let a = +window.a.value;
-    let b = +window.b.value;
-    let c = +window.c.value;
-    let result = solveEquation(a,b,c);
-    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
-    let span = window.result;
-    span.textContent = "х = "+JSON.stringify(result)
-    return arr;;
-}
+    let arr = [];
+    let d = b**2-4*a*c;
+    if (d > 0) {
+      console.log(`Дискриминант ${d} > 0, 2 корня`);
+      let x1 = (-b + Math.sqrt(d))/(2*a);
+      let x2 = (-b - Math.sqrt(d))/(2*a);
+      arr.push(x1, x2);
+    } else if (d == 0) {
+      console.log(`Дискриминант ${d} = 0, 1 корень`);
+        let x1 = -b/(2*a);
+        arr.push(x1);
+    }      else if (d < 0) {
+          console.log(`Дискриминант ${d} < 0, корней нет`);
+    }
+     console.log(arr);
+    return arr;
+  }
+  
+  solveEquation(1,2,4);
